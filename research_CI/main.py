e6300a61bdb3b153dea93f3e9839e71a20967170
cryptosalamander <dhy03196@naver.com>
2021-10-04 09:25:53 +0000

    [MOD]Reformatting for PR

--- research_CI/main.py
+++ research_CI/main.py
@@ -1,4 +1,6 @@
-def        helloworld(a):# hello the o         world!
+def helloworld(a):  # hello the o         world!
     print(f"Hello World! {a}!")
+
+
 if __name__ == "__main__":
     helloworld("Luke")
